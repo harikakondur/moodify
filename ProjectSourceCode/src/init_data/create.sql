1e5b779813cdb4e2eb22d27d84ba1d595cf28d01
RenePunch <121848199+RenePunch@users.noreply.github.com>
2024-04-09 16:10:09 -0600

    version 1.00

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,8 +1,22 @@
-drop table if exists users;
-create table users(
+create table if not exists users(
     id serial primary key,
     spotifyuser varchar(50) not null,
     password char(60) not null
 );
+
+create table if not exists playlists(
+    playlist_id serial primary key,
+    users int references users(id)
+);
+
+create table if not exists playlist_mood(
+    playlist_id int references playlists(playlist_id),
+    mood1 int not null,
+    mood2 int not null,
+    mood3 int not null,
+    mood4 int not null,
+    mood5 int not null
+);
+
 
 
